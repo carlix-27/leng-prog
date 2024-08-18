02428fb279e7d80692d4da5c1d16c33b21c94984
carlix-27 <128094132+carlix-27@users.noreply.github.com>
2024-08-18 17:33:08 -0300

    charToBits done

--- haskell-exercises/lists2/src/ListsPart2.hs
+++ haskell-exercises/lists2/src/ListsPart2.hs
@@ -7,16 +7,18 @@ data Bit = F | T  deriving (Eq, Show, Enum, Read)
 type Bits = [Bit]
 
 bitAt :: Int -> Char -> Bit
-bitAt n c = if testBit (ord c) (7-n) then T else F 
+bitAt n c = if testBit (ord c) (7-n) then T else F -- Con el testBit chequea que este activado o no. T-> 1; F -> 0
 
-charToBits :: Char -> Bits
-charToBits = error "Implement It"
+-- ord c -> 'A' -> 65 -> '01000001' -> F T F F F F F T
+
+charToBits :: Char -> Bits -- Bits = [Bit]
+charToBits c = [bitAt n c | n <- [0 .. 7]]
 
 
-bits::String -> Bits
-bits = error "Implement It"
+bits:: String -> Bits -- String -> [Char]
+bits (c: cs) = map (foldr (charToBits c))  cs
 
 type Solution = [Int]
 
-queens::Int -> [Solution]
-queens error "Implement It"
+queens:: Int -> [Solution]
+queens = error "Implement It"
