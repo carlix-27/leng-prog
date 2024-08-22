5df5ed294fd66c3091c50e6ced8704021a1b66fe
carlix-27 <128094132+carlix-27@users.noreply.github.com>
2024-08-22 17:13:49 -0300

    find Done

--- haskell-exercises/trie/src/Trie.hs
+++ haskell-exercises/trie/src/Trie.hs
@@ -2,10 +2,10 @@ module Trie  (Trie(..), left, right, find, decode, toList) where
 
 import Bit
   
-data Trie a =  Leaf Char | Trie a :-: Trie a deriving(Eq, Show) -- Add the Type definition deriving (Eq, Show)
+data Trie a =  Leaf a | Trie a :-: Trie a deriving(Eq, Show) -- Add the Type definition deriving (Eq, Show)
 
 -- A :-: B
--- Las hojas (Leaf) son los caracteres. Por como funciona, solo tenemos datos en las hojas de este arbol.
+-- Las hojas (Leaf) son los datos. Por como funciona, solo tenemos datos en las hojas de este arbol.
 -- Trie :-: Trie es como deberian estructurarse.
 
 buildTrie:: Trie a -> Trie a -> Trie a
@@ -13,15 +13,21 @@ buildTrie a b = a :-: b
 
 left::Trie a -> Trie a
 left (nodeLeft :-: _) = nodeLeft -- Te da el nodo izquierdo (contiene la informacion)
+left (Leaf a :-: _) = error "left of Leaf" -- Maneja el error si analiza un nodo left de un node que ya es un leaf. Condicion de corte del analisis.
 
--- Como se manejaria el caso del error?
--- left ('' :-:_) = error "Left of: Leaf" -- Maneja el error si analiza un nodo left que ya es un leaf. Condicion de corte del analisis.
+
 
 right::Trie a -> Trie a
 right (_ :-: nodeRight) = nodeRight
-  
+right (_ :-: Leaf a) = error "right of Leaf"
+
+
+
 find::Bits -> Trie a -> a
-find = error "Define it"
+find [] (Leaf a) = a -- Caso base, donde llega a una hoja. Devuelve el caracter que tiene asociado esa hoja.
+find (bit: bits) trie
+     | bit == F = find bits (left trie)
+     | bit == T = find bits (right trie)
 
 decode::Bits -> Trie Char -> String
 decode = error "Define it"
