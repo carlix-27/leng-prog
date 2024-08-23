24a03c7baa9629312303a3c00569a91de94f3651
carlix-27 <128094132+carlix-27@users.noreply.github.com>
2024-08-23 18:48:48 -0300

    Trying to resolve

--- haskell-exercises/trie/src/Trie.hs
+++ haskell-exercises/trie/src/Trie.hs
@@ -1,8 +1,10 @@
 module Trie  (Trie(..), left, right, find, decode, toList) where
 
 import Bit
+import Data.Char(chr)
 
 data Trie a =  Leaf a | Trie a :-: Trie a deriving(Eq, Show) -- Add the Type definition deriving (Eq, Show)
+
 
 -- A :-: B
 -- Las hojas (Leaf) son los datos. Por como funciona, solo tenemos datos en las hojas de este arbol.
@@ -12,37 +14,55 @@ buildTrie:: Trie a -> Trie a -> Trie a
 buildTrie a b = a :-: b
 
 left::Trie a -> Trie a
-left (nodeLeft :-: _) = nodeLeft -- Te da el nodo izquierdo (contiene la informacion)
-left (Leaf a :-: _) = error "left of Leaf" -- Maneja el error si analiza un nodo left de un node que ya es un leaf. Condicion de corte del analisis.
+left (nodeLeft :-: _) = nodeLeft -- Te da el nodo izquierdo (contiene la informacion). Puede tener hijos, como no.
+left (Leaf a :-: _) = Leaf a
+-- left (Leaf a :-: _) = error "left of Leaf" -- Maneja el error si analiza un nodo left de un node que ya es un leaf. Condicion de corte del analisis.
 
 
 right::Trie a -> Trie a
 right (_ :-: nodeRight) = nodeRight
-right (_ :-: Leaf a) = error "right of Leaf"
+right ( _ :-: Leaf a) = Leaf a
+-- right (_ :-: Leaf a) = error "right of Leaf"
 
 
 find::Bits -> Trie a -> a
 find [] (Leaf a) = a -- Caso base, donde llega a una hoja. Devuelve el caracter que tiene asociado esa hoja.
+-- find _ (Leaf a) = a --error "This element not exist in this Binary Trie"-- Caso donde se pida algo que no existe en el arbol.
 find (bit: bits) trie
      | bit == F = find bits (left trie)
      | bit == T = find bits (right trie)
 
 
+
+-- Como resulta decode, si lo encaro por este lado?
 charAt:: Bit -> Char -- El bit es F o es T. La transformacion numerica de esa cadena, la puedo hacer segun 0 o 1. Y ese conjunto de numbers. Pasarlo al number correspondiente. Y luego al char.
-chatAt bit =
-       where
-         listOfN = []
-         | bit == F = 0: listOfN
-         | bit == T = 1: listOfN
-         number = bitsToInt listOfN
-         chr(number)
+charAt bit =
+       let listOfN = case bit of
+                   F -> [0]
+                   T -> [1]
+       in chr (bitsToInt listOfN) -- Pasa el numero hallado a caracter.
 
 bitsToInt:: [Int] -> Int -- Teorema fundamental de la numeracion para pasar la representacion binaria en un nro.
-bitsToInt list = foldl(\acc bit -> acc * 2 + bit) 0 -- Chequea bien como hace esto. acc que es? Un acumulador que pasamos inicialmente en 0. Y vamos sumando el valor entero del bit.
+bitsToInt list = bitsToIntAux list (length(list) - 1)
+
+bitsToIntAux :: [Int] -> Int -> Int
+bitsToIntAux [] _ = 0
+bitsToIntAux (n:ls) c =  n * 2^c  +  bitsToIntAux (ls) (c - 1)
+
+-- bitsToInt list = foldl (\acc bit -> acc * 2 + bit) 0 -- Chequea bien como hace esto. acc que es? Un acumulador que pasamos inicialmente en 0. Y vamos sumando el valor entero del bit. CG
 
 
-decode::Bits -> Trie Char -> String
-decode = error "Define it"
+decode::Bits -> Trie Char -> String -- String - [Char]
+decode [] _ = [] -- Le ponemos esto para que sepa cuando cortar.
+decode bits trie =
+          let
+            character = find bits trie
+            anotherBits = tail bits -- Debe analizar el resto de la lista para evitar que tengamos el mismo caracter nuevamente. Pidiendo el tail, devuelve todos los elementos menos el head.
+
+          in character : (decode anotherBits trie) -- Tengo que tener en cuenta que siga analizando la cadena de bits que falta analizar
+
+
+
 
 toList::Trie a -> [(a, Bits)]
 toList = error "Define it"
