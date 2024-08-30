c538659caab9cabac1fb92552d1874a76b0c499a
carlix-27 <128094132+carlix-27@users.noreply.github.com>
2024-08-30 12:24:20 -0300

    updates frequencies
    La logica va queriendo, pero aun no esta del todo bien refinado.

--- haskell-exercises/frequencies/src/Frequencies.hs
+++ haskell-exercises/frequencies/src/Frequencies.hs
@@ -7,7 +7,14 @@ import Data.Tuple(swap)
 type Frequency = (Int, Char)
 
 frequencies::String -> [Frequency]
-frequencies = error "Implement it"
+frequencies [] = []
+frequencies (c:cs) = frequenciesAux c cs 1
+
+frequenciesAux:: Char -> String -> Int -> [Frequency] -- TODO. Hay algo mal en la combinacion de las listas.
+frequenciesAux currentChar [] count = [(count, currentChar)]
+frequenciesAux currentChar (c: cs) count
+            | currentChar == c = frequenciesAux currentChar cs (count + 1)
+            | currentChar /= c = (count, currentChar) : frequenciesAux c cs 1 -- Con esta logica, manejo que cambie el currentChar.
 
 frequencyMap::(Ord a) => [a] -> Map a Int
 frequencyMap = error "Implement it"
--- haskell-exercises/frequencies/src/Trie.hs
+++ haskell-exercises/frequencies/src/Trie.hs
@@ -8,16 +8,41 @@ data Trie a = Leaf a
 -- Bring definition from previous TP
 
 left::Trie a -> Trie a
-left = error "Define it"
+left (nodeLeft :-: _) = nodeLeft
+left (Leaf a :-: _) = Leaf a
 
 right::Trie a -> Trie a
-left = error "Define it"
+right (_ :-: nodeRight) = nodeRight
+right ( _ :-: Leaf a) = Leaf a
   
 find::Bits -> Trie a -> a
-find = error "Define it"
+find [] (Leaf a) = a
+find (bit: bits) (Leaf a) = a
+find (bit: bits) trie
+     | bit == F = find bits (left trie)
+     | bit == T = find bits (right trie)
 
 decode::Bits -> Trie Char -> String
-decode = error "Define it"
+decode [] _ = []
+decode bits trie =
+          let
+            (character, anotherBits) = findDecode bits trie
+
+          in character : (decode anotherBits trie)
+
+
+findDecode::Bits -> Trie a -> (a, Bits)
+findDecode [] (Leaf a) = (a, [])
+findDecode bits (Leaf a) = (a, bits)
+findDecode (bit: bits) trie
+     | bit == F = findDecode bits (left trie)
+     | bit == T = findDecode bits (right trie)
 
 toList::Trie a -> [(a, Bits)]
-toList = error "Define it"            
+toList (Leaf a) = []
+toList trie = travelTrie trie []
+
+travelTrie:: Trie a -> Bits -> [(a, Bits)]
+travelTrie (Leaf a) bits = [(a, bits)]
+travelTrie trie bits = travelTrie (left trie) (bits ++ [F]) ++ travelTrie (right trie) (bits ++ [T])
+
