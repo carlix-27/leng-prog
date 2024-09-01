b8d2693bec17eb810040a895b765d101ca16e785
carlix-27 <128094132+carlix-27@users.noreply.github.com>
2024-09-01 20:45:58 -0300

    Update Frequencies.hs

--- haskell-exercises/frequencies/src/Frequencies.hs
+++ haskell-exercises/frequencies/src/Frequencies.hs
@@ -2,19 +2,10 @@ module Frequencies  (Frequency, frequencyMap, frequencies, insert, insertionSort
 
 import Data.Map(Map)
 import qualified Data.Map as Map
-import Data.Tuple(swap)
+import Data.Tuple(swap) -- Siempre estuvo?
 
 type Frequency = (Int, Char)
 
-frequencies::String -> [Frequency]
-frequencies [] = []
-frequencies (c:cs) = frequenciesAux c cs 1
-
-frequenciesAux:: Char -> String -> Int -> [Frequency] -- TODO. Hay algo mal en la combinacion de las listas.
-frequenciesAux currentChar [] count = [(count, currentChar)]
-frequenciesAux currentChar (c: cs) count
-            | currentChar == c = frequenciesAux currentChar cs (count + 1)
-            | currentChar /= c = (count, currentChar) : frequenciesAux c cs 1 -- Con esta logica, manejo que cambie el currentChar.
 
 frequencyMap::(Ord a) => [a] -> Map a Int
 frequencyMap = error "Implement it"
@@ -24,3 +15,23 @@ insert = error "Implement it"
 
 insertionSort :: (Ord a) => [a] -> [a]
 insertionSort = error "Implement it"
+
+
+frequencies::String -> [Frequency]
+frequencies [] = []
+frequencies (c:cs) = checkValues(frequenciesAux c cs 1)
+
+frequenciesAux:: Char -> String -> Int -> [Frequency] -- TODO. Hay algo mal en la combinacion de las listas.
+frequenciesAux currentChar [] count = [(count, currentChar)]
+frequenciesAux currentChar (c: cs) count
+            | currentChar == c = frequenciesAux currentChar cs (count + 1)
+            | currentChar /= c = (count, currentChar) : frequenciesAux c cs 1 -- Con esta logica, manejo que cambie el currentChar.
+
+checkValues:: [Frequency] -> [Frequency] -- TODO: La logica no es correcta, llegamos al mismo resultado. Por como esta hecha la comparacion.
+checkValues [] = []
+checkValues (tuple: []) = [tuple]
+checkValues (tuple: tuple2: rest)
+          | snd tuple == snd tuple2 = ((fst tuple + fst tuple2), snd tuple): checkValues (tuple2: rest)
+          | otherwise = tuple : checkValues (tuple2: rest)
+
+
