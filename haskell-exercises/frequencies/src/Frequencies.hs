eecf100a4bd8841c4b10d02d9f58e3c116d53d1b
carlix-27 <128094132+carlix-27@users.noreply.github.com>
2024-09-12 14:25:15 -0300

    haciendo algunos metodos

--- haskell-exercises/frequencies/src/Frequencies.hs
+++ haskell-exercises/frequencies/src/Frequencies.hs
@@ -2,22 +2,44 @@ module Frequencies  (Frequency, frequencyMap, frequencies, insert, insertionSort
 
 import Data.Map(Map)
 import qualified Data.Map as Map
-import Data.Tuple(swap) -- Siempre estuvo?
+import Data.Tuple(swap)
 
 type Frequency = (Int, Char)
 
 
 frequencyMap::(Ord a) => [a] -> Map a Int
-frequencyMap = error "Implement it"
+frequencyMap [] = Map.empty
+frequencyMap (a: as) = frequencyMapAux a as 1
+
+-- En este caso: a es el elemento. as es el resto de la lista, 1 es el contador de ocurrencias
+frequencyMapAux::(Ord a) => a -> [a] -> Int -> Map a Int
+frequencyMapAux currentElement [] count = Map.singleton currentElement count -- Map.fromList[(currentElement, count)]
+frequencyMapAux currentElement (e: as) count
+                | currentElement == e =  Map.insert currentElement (count + 1) (frequencyMapAux e as 1) -- Pisa el valor de la clave especificada.
+                | otherwise = frequencyMapAux currentElement as count  -- Sigue analizando. Pero desde el current element. Para analizar todos los caracteres del arreglo.
+
+-- Es posible que aca, tengas que usar insert? No lo se. Creo que tu implementacion va encaminada pero falta algo..
+
+
+
+
 
 insert::(Ord a) => a -> [a] -> [a]
-insert = error "Implement it"
+insert x [] = x : []
+insert x (y:ys)
+    | x <= y = x:y:ys
+    | otherwise = y: (insert x ys)
 
 insertionSort :: (Ord a) => [a] -> [a]
-insertionSort = error "Implement it"
+insertionSort [] = []
+insertionSort [x] = [x]
+insertionSort (x: y: ys)
+           | y < x = insertionSort(y: (insertionSort(x: ys)))
+           | x < y = x: (insertionSort(y:ys))
+           | otherwise = insert x (insertionSort (y:ys))
 
 
-frequencies::String -> [Frequency]
+frequencies::String -> [Frequency] -- Es posible usar insert y frequencyMap.
 frequencies [] = []
 frequencies (c:cs) = checkValues(frequenciesAux c cs 1)
 
