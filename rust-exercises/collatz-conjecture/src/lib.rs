8f5d759ad14ec7c99ea67f0500f113cd4602c251
carlix-27 <128094132+carlix-27@users.noreply.github.com>
2024-10-07 11:55:48 -0300

    Collatz Conjecture done.

--- rust-exercises/collatz-conjecture/src/lib.rs
+++ rust-exercises/collatz-conjecture/src/lib.rs
@@ -1,4 +1,26 @@
-pub fn collatz(mut n: u64) -> Option<u64> {
-  None
+
+pub fn collatz(n: u64) -> Option<u64> {
+    collatz_aux(n, 0)
+}
+
+pub fn collatz_aux(n: u64, steps: u64) -> Option<u64>{
+    if n <= 0 {
+        None
+    } else if n == 1 {
+        Some(steps)
+    } else if check_parity(n) == true {
+        collatz_aux(n/2, steps + 1)
+
+    } else {
+        collatz_aux(3*n+1, steps + 1)
+    }
+}
+
+pub fn check_parity(n: u64) -> bool {
+    if n % 2 == 0{
+        true
+    } else{
+        false
+    }
 }
 
