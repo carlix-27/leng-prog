ec65627e44da9d5303660d9a43f43126fb4bc47e
carlix-27 <128094132+carlix-27@users.noreply.github.com>
2024-10-07 13:40:20 -0300

    Fraction Type done.

--- rust-exercises/fraction_type/src/lib.rs
+++ rust-exercises/fraction_type/src/lib.rs
@@ -3,14 +3,34 @@ use std::ops;
 #[derive(Debug, PartialEq, Eq)]
 pub struct Fraction(pub i32, pub i32);
 
+// impl of Struct
 impl Fraction {
-    
+    pub fn add(self, other: Fraction) -> Fraction {
+        simplify(self.0 * other.1 + other.0 * self.1, other.1 * self.1)
+    }
+
+    // How to use this? -> With self, and other.
+    pub fn sub(self, other: Fraction) -> Fraction {
+        simplify(self.0 * other.1 - other.0 * self.1, self.1 * other.1)
+    }
+
+    pub fn mul(self, other: Fraction) -> Fraction {
+        simplify(self.0 * other.0, self.1 * other.1)
+    }
+
+    pub fn divide(self, other: Fraction) -> Fraction {
+        simplify(self.0 * other.1, self.1 * other.0)
+    }
 }
 
-impl ops::Add for Fraction {
-    type Output = Fraction;
 
-    
+/// Add is a Trait
+impl ops::Add for Fraction {
+    type Output = Fraction; // This is result of add fractions.
+
+    fn add(self, other: Fraction) -> Fraction{ // TODO: What is difference between self and &self?
+        Fraction::add(self, other)
+    }
 }
 
 /// Calculate the Highest common factor between 2 numbers
