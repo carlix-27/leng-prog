e5f8f7cf65b120edac60e2e9a43cef22665cbc28
carlix-27 <128094132+carlix-27@users.noreply.github.com>
2024-10-07 14:21:52 -0300

    Health statistics done.

--- rust-exercises/health-statistics/src/lib.rs
+++ rust-exercises/health-statistics/src/lib.rs
@@ -5,15 +5,33 @@ pub struct User {
 }
 
 impl User {
-    pub fn new(name: &str, age: u32, weight: f32) -> Self { panic!("Implement me!") }
 
-    pub fn name(&self) -> &str { panic!("Implement me!") }
+    // Returns a new instance of User
+    pub fn new(name: &str, age: u32, weight: f32) -> Self {
+        User{
+            name: String::from(name),
+            age,
+            weight
+        }
+    }
 
-    pub fn age(&self) -> u32 { panic!("Implement me!") }
+    pub fn name(&self) -> &str {
+        self.name.as_str() // Throw a &str from String. In doc: 'Extracts a string slice containing the entire String'.
+    }
 
-    pub fn weight(&self) -> f32 { panic!("Implement me!") }
+    pub fn age(&self) -> u32 {
+        self.age
+    }
 
-    pub fn set_age(&mut self, new_age: u32) { panic!("Implement me!") }
+    pub fn weight(&self) -> f32 {
+        self.weight
+    }
 
-    pub fn set_weight(&mut self, new_weight: f32) { panic!("Implement me!") }
+    pub fn set_age(&mut self, new_age: u32) { // Self is mutable -> (age -> new_age) with the same instance of User. Bob now, be 33 years old.
+        self.age = new_age
+    }
+
+    pub fn set_weight(&mut self, new_weight: f32) {
+        self.weight = new_weight
+    }
 }
