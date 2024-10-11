60d7c2ec3e1ab3bbd070ab9b2c92911c093d1b68
carlix-27 <128094132+carlix-27@users.noreply.github.com>
2024-10-11 12:45:39 -0300

    Queen Attack Done

--- rust-exercises/queen-attack/src/lib.rs
+++ rust-exercises/queen-attack/src/lib.rs
@@ -12,7 +12,7 @@ pub struct Queen {
 impl ChessPosition {
     pub fn new(rank: i32, file: i32) -> Option<Self> {
         // Check invalid move
-        if rank <= 0 || file <= 0 || rank >= 8 || file >= 8 {
+        if rank < 0 || file < 0 || rank >= 8 || file >= 8 { // Both Rank and File between 0 and 7.
             None
         } else{
             Some(ChessPosition(rank, file)) // Check if position is some
@@ -23,10 +23,8 @@ impl ChessPosition {
 impl Queen {
     // if arrive to this part of the implementation -> position is valid. because we check invalid position previously with ChessPosition method.
     pub fn new(position: ChessPosition) -> Self {
-        let pos = position;
+        Queen { position } // Return an instance of Queen with a valid position.
     }
-
-
 
     // I have two queens 'como' parameters.
     pub fn can_attack(&self, other: &Queen) -> bool {
