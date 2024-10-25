9536c8f28f5ce88466bed82ac03e22c312b65f4e
carlix-27 <128094132+carlix-27@users.noreply.github.com>
2024-10-25 12:54:57 -0300

    tp9 Done

--- rust-exercises/sum-of-multiples/src/lib.rs
+++ rust-exercises/sum-of-multiples/src/lib.rs
@@ -1,3 +1,28 @@
+
+// Tip: ver las funciones de listas que van a ayudar bastante para este ejercicio.
+
 pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
-    0
+    let vec = 0.. limit;
+    let total: u32 = vec.filter(|i| take_multiplies(&i, factors)).sum(); // Me trae un vector nuevo porque es inmutable.
+    total
 }
+
+pub fn take_multiplies(i: &u32, factors: &[u32]) -> bool {
+    // check multiplies with factors elements. Me quedo con los elementos que cumplan. filter, si cumple se queda.
+    factors.iter().any(|&v| v != 0 && i % v == 0) // v es cada valor del factors porque hace la iteracion ahi.
+    // Primero chequea la primer condicion, y luego la condicion de i
+    // Recordemos que por como funciona and, con que uno no pase, ya es suficiente.
+}
+
+// factors.iter().any(|&v| i % v == 0 && v != 0 ) // v es cada valor del factors porque hace la iteracion ahi.
+// Asi rompe porque en primer instancia hace la division con v, y no puedo hacer eso porque tu v es 0.
+
+// analizando los tests, vemos que si pasamos como factor 0, i esta valiendo 0 tambien, por como hace el chequeo
+// devuelve false mi any, y por tanto solo tipo el valor que quedo de i, que es 0.
+
+// Todo: Lo que me queda haciendo ruido, es como reacciona ante la lista vacia.
+
+
+// Aclaraciones
+// borrowing evita problemas de ownership.
+// En el caso del Rc podes tener multiples referencias, sin la necesidad de perder ownership debido al uso del reference counted.
--- rust-exercises/word-count/src/lib.rs
+++ rust-exercises/word-count/src/lib.rs
@@ -2,5 +2,12 @@ use std::collections::HashMap;
 
 /// Count occurrences of words.
 pub fn word_count(phrase: &str) -> HashMap<String, u32> {
-  panic!("Implement me!")
+  let mut map = HashMap::new();
+  for word in phrase.split_whitespace(){
+      let count = map.entry(String::from(word.to_lowercase())).or_insert(0);
+      *count += 1;
+  }
+  map
 }
+
+// TODO: pensalo de otra forma, para evitar usar el for. Quiza con un punto iter, y una funcion lambda, podria funcionar.
