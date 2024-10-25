bd947655735d75f53f86a5692b66c60870806db5
carlix-27 <128094132+carlix-27@users.noreply.github.com>
2024-10-25 20:09:37 -0300

    tp10 Done

--- rust-exercises/circular-buffer/src/lib.rs
+++ rust-exercises/circular-buffer/src/lib.rs
@@ -19,23 +19,40 @@ impl<T:Clone> CircularBuffer<T> {
         }
     }
 
+    // Utilizar el hint de decremento
     pub fn read(&mut self) -> Result<T, Error> {
-        todo!()
+        let index = (self.next + self.buffer.len() - self.size) % self.buffer.len();
+        if let Some(value) = self.buffer[index].take(){ // Tomo el valor del option
+            self.size -= 1; // Descremento el size de mi buffer
+            return Ok(value)
+        }
+        Err(Error::EmptyBuffer)
     }
 
+    // Utilizar el hint de incremento
     pub fn write(&mut self, byte: T) -> Result<(), Error> {
-        todo!()
+        if self.is_full() {
+           return Err(Error::FullBuffer)
+        }
+        self.buffer[self.next] = Some(byte); // Aca agregamos el elemento teniendo en cuenta el next. (siguiente slot).
+        self.next = (self.next + 1) % self.buffer.len(); // Hint, para el incremento circular
+        self.size += 1;
+        Ok(())
     }
 
     pub fn clear(&mut self) {
-        todo!()
+        for slot in self.buffer.iter_mut(){
+            *slot = None; // modifica el valor para que todos los elementos se anulen.
+        }
+        self.size = 0;
+        self.next = 0;
     }
 
     pub fn is_empty(&self) -> bool {
-        todo!()
+        self.size == 0
     }
 
     pub fn is_full(&self) -> bool {
-        todo!()
+        self.buffer.capacity() == self.size
     }
 }
