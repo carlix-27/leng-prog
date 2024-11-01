4989e508806f5b5cbdf3a098252d18d215d96bcf
carlix-27 <128094132+carlix-27@users.noreply.github.com>
2024-11-01 20:36:47 -0300

    Swap done

--- rust-exercises/memory-allocation/src/main.rs
+++ rust-exercises/memory-allocation/src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
     println!("Hello, world!");
 }
+
+// Consigna
+// Dibuja un esquema simple del Stack y el Heap para el siguiente código.
+
+fn test(a: i32) {
+    if a == 0 { return }
+    let b = a - 1;
+    let c = &b;
+    let d = Box::new(*c + 10);
+
+    test(b);
+    println!("{a} {b} {c} {d}")
+}
+
+// Cuando se invoca con test(2)
--- rust-exercises/swap/src/main.rs
+++ rust-exercises/swap/src/main.rs
@@ -1,3 +1,19 @@
+// Escribir la funcion swap, que intercambia los valores entre dos variables, Por ejemplo:
+
+use std::ptr::addr_of_mut;
+
 fn main() {
-    println!("Hello, world!");
+    let mut x = 100;
+    let mut y = 200;
+    swap(&mut x, &mut y);
+    println!("x= {x}, y= {y}")
+}
+
+// Deberia imprimir
+// x= 200, y= 100
+
+pub fn swap(x: &mut i32, y: &mut i32){
+    let tmp = *x; // Guardo x
+    *x = *y;           // Cambia x a y.
+    *y = tmp           // Cambia y a x.
 }
